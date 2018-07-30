340cfa8b4540a1d8fbebf1f2bb1e199ee791e5cc
Byron.wang <stormlandon@outlook.com>
2018-07-30 20:23:19 +0800

    init repo

---
+++ lisp/init-kuma.el
@@ -0,0 +1,31 @@
+;; 高亮当前行
+(global-hl-line-mode t)
+
+(global-linum-mode t) ;; enable line numbers globally
+(column-number-mode 1)
+
+
+
+
+(display-time-mode t)
+(setq display-time-24hr-format 1)
+
+(display-battery-mode t)
+
+(require 'sr-speedbar)
+
+
+(set-scroll-bar-mode nil)
+
+(put 'downcase-region 'disabled nil)
+;;----------global-key------------
+
+(global-set-key (kbd "C->") 'other-window) ;; set hotkey change window
+
+;; Magit shortcut
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+(fset 'yes-or-no-p 'y-or-n-p) ;; y or n instead of yes or no
+
+(provide 'init-kuma)
