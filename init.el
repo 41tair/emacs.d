a0a21c842eb9b3b47cfee86bf9b3f70ddeef341b
Byron.wang <stormlandon@outlook.com>
2018-07-30 21:08:48 +0800

    add init-csv
    Modified-by: Byron.wang <stormlandon@outlook.com>

--- init.el
+++ init.el
@@ -19,7 +19,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ag multi-term magit sr-speedbar fullframe)))
+ '(package-selected-packages
+   (quote
+    (csv-mode ag multi-term magit sr-speedbar fullframe)))
  '(term-default-bg-color "#000000")
  '(term-default-fg-color "#dddd00"))
 (custom-set-faces
