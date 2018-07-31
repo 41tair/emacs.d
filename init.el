618d30dbe61a84ce1822e9109da896c57d2f9ee1
Byron.wang <stormlandon@outlook.com>
2018-07-31 16:41:12 +0800

    add ibuffer

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 ;;(require 'init-session)
 (require 'init-themes)
 (require 'init-jira)
+(require 'init-ibuffer)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
