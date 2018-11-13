6abfba6275ff81b6c6e57f23a75ab8bbcaf78002
Byron.wang <stormlandon@outlook.com>
2018-11-13 17:51:31 +0800

    update exec path for macOS

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -5,10 +5,18 @@
     (add-to-list 'exec-path-from-shell-variables var)))
 
 
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-(exec-path-from-shell-copy-env "GOPATH")
+;; (when (memq window-system '(mac ns x))
+;;   (exec-path-from-shell-initialize))
+;; (exec-path-from-shell-copy-envs '("GOPATH" "LANG" "GPG_AGENT_INFO" "SSH_AUTH_SOCK"))
+(when (and (memq window-system '(mac ns))
+           (require 'exec-path-from-shell nil t))
+  ;; (setq exec-path-from-shell-debug t)
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs '("LANG" "GPG_AGENT_INFO" "SSH_AUTH_SOCK" "GOPATH"))
+  (message "Initialized PATH and other variables from SHELL."))
 
 (when (eq system-type 'darwin)
   (add-to-list 'load-path "/usr/local/Cellar/mu/HEAD/share/emacs/site-lisp/mu4e"))
+
+(setq exec-path-from-shell-check-startup-files nil) 
 (provide 'init-exec-path)
