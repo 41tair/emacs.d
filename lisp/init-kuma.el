a137ff1bcfff7b9462fa0f837149d9cbac67f8f6
Byron.wang <stormlandon@outlook.com>
2018-11-15 18:58:26 +0800

    add font settings

--- lisp/init-kuma.el
+++ lisp/init-kuma.el
@@ -47,6 +47,21 @@
 (setq visible-bell 0)
 
 (global-undo-tree-mode)
+
+
+;; chinese font width
+(setq fonts
+      (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
+            ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
+            ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
+(set-face-attribute 'default nil :font
+                    (format "%s:pixelsize=%d" (car fonts) 12))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font (frame-parameter nil 'font) charset
+                    (font-spec :family (car (cdr fonts)))))
+;; Fix chinese font width and rescale
+(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro Hei Mono" . 1.2) ("STHeiti". 1.2)))
+
+
 (require 'cc-mode)
 (provide 'init-kuma)
-
