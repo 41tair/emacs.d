4bb324edb5cf92739343e8eef9d240ceaa8aa03f
Byron.wang <stormlandon@outlook.com>
2018-11-15 19:03:33 +0800

    add yasnippet

--- init.el
+++ init.el
@@ -24,11 +24,12 @@
 (require 'init-org)
 ;;(require 'init-session)
 (require 'init-themes)
-(require 'init-jira)
+;;(require 'init-jira)
 (require 'init-ibuffer)
 (require 'init-exec-path)
 (require 'init-markdown)
 (require 'init-mu4e)
+(require 'init-yasnippet)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
