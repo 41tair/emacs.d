d93a06b6dda58777922eb8e6e90688aeb8a9a0d3
Byron Wang <stormlandon@outlook.com>
2019-11-03 22:15:35 +0800

    delete unsed code

--- lisp/init-kuma.el
+++ lisp/init-kuma.el
@@ -18,7 +18,7 @@
 
 (setq make-backup-files nil)
 
-(setq initial-scratch-message ";;Talk is cheap")
+(setq initial-scratch-message ";;Talk is cheapaaaaaaaaaaaaaaaaaaa")
 
 (set-scroll-bar-mode nil)
 
@@ -27,7 +27,7 @@
 (defun insertsshkey (string)
   "Insert ssh key"
   (interactive "sInsert ssh key")
-  (insert "mkdir -p ~/.ssh;touch ~/.ssh/authorized_keys;echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9GGP6aYJedzOK68wrqreRuezjS0KJ+CXge93i0ZYdxNqPL6snJcsm1i+MZJXFaqf3MQOfwcy72ijnHbC/iCF9sHAHzrOzviUpLugjO/b8pk7QoJ0ZMueQwDb+MqakS5NNBxXaxzz0d608VibzBRTgGcq4y2MIo53GEuSZZh7oNd4SN02QGHNGviudaCII2gc++thutnP2UZLQPqbWkjUjikW9LSScXGPtKII3wg4uwRVHRCz/2V99QTpG5et+OTvjr90817h2p7+8bCAX3qityapRXFVvKfpmhek18KTTqxanzMcyLpO80PQyTXU2Vu3wUUPIdhW75sc4eJclnbp3 altair96wby@gmail.com'>> ~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys")
+  (insert "mkdir -p ~/.ssh;touch ~/.ssh/authorized_keys;echo ''>> ~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys")
   (term-send-input))
 
 ;;----------global-key------------
@@ -41,7 +41,6 @@
 
 (global-set-key [f12] 'goto-line)
 (global-set-key [f9] 'insertsshkey)
-(global-set-key [f2] 'mu4e)
 (global-set-key [f3] 'execute-extended-command)
 (global-set-key [f4] 'ag-project)
 (global-set-key [f5] 'kmacro-start-macro)
@@ -52,25 +51,10 @@
 
 (global-undo-tree-mode)
 
-
-;; chinese font width
-(setq fonts
-      (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
-            ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
-            ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
-(set-face-attribute 'default nil :font
-                    (format "%s:pixelsize=%d" (car fonts) 12))
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font) charset
-                    (font-spec :family (car (cdr fonts)))))
-;; Fix chinese font width and rescale
-(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro Hei Mono" . 1.2) ("STHeiti". 1.2)))
-
-
-(with-eval-after-load "magit-fetch"
-(magit-define-popup-switch
- 'magit-fetch-popup
- ?t "Fetch tags from remote" "--tags"))
+;; (with-eval-after-load "magit-fetch"
+;; (magit-define-popup-switch
+;;  'magit-fetch-popup
+;;  ?t "Fetch tags from remote" "--tags"))
 
 
 (show-paren-mode t)
